1bfa7a09743c5ac0b4affa5b8d924ddd4d535c1b
Shahidur Rahman <166603336+srshoruv@users.noreply.github.com>
2025-04-27 22:40:13 +0600

    some patterns

---
+++ patternPrinting.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int z = 0; z <= i; z++)
+        {
+            for (int j = 1; j >= 1; j--)
+            {
+                cout << j << " ";
+            }
+
+        }
+        cout << endl;
+
+        
+    }
+}
